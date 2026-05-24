1ec345fe09f543777845e2fb1d28f57b57be7b9d
ahmedshenawy1997 <ahmedshino99@gmail.com>
2026-05-24 15:37:32 +0300

    add seed to startup

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,3 +1,6 @@
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = 'postgresql://postgres:tMpfKsQwrOwSWzxxHwvsPAlFMNrkxRTF@postgres.railway.internal:5432/railway';
+}
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 
